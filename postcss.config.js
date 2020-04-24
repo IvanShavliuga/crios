225d7d7822f394ade8a3e5736eac9dfbe4856030
Ivan Shavliuga <ivanov-R0M@yandex.ru>
2020-04-24 14:31:36 +0300

    delete postcss

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    require('precss'),
-    require('autoprefixer'),
-    require('postcss-simple-vars')({
-      variables: require('./src/css/variables.css.js')
-    }),
-  ]
-}
